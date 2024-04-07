61d76740d93312712ce872ad936c21cc405f505c
marc jaderson <jadersonmarc@gmail.com>
2024-04-07 02:26:31 -0300

    fix

--- src/components/SimulationForm.js
+++ src/components/SimulationForm.js
@@ -1,10 +1,16 @@
-function SimulationForm({ onSubmit, register }) {
+import { useForm } from 'react-hook-form';
+
+function SimulationForm() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = data => {
+        console.log(data); };
     return (
       <div className="bg-white m-8 p-8 rounded-md">
         <h3 className="text-left text-xl font-roboto font-bold leading-9">
           Selecione um veículo que deseja simular o financiamento
         </h3>
-        <form onSubmit={onSubmit} className="lg:flex my-6 items-center max-sm:space-y-4 lg:space-x-6">
+        <form onSubmit={handleSubmit(onSubmit)} className="lg:flex my-6 items-center max-sm:space-y-4 lg:space-x-6">
           <select {...register("selectOption")} className="w-60 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5">
             <option value="option1">Opção 1</option>
             <option value="option2">Opção 2</option>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,19 @@
-import Image from "next/image";
 import Header from '../components/Header';
 import SimulationHeader from '../components/SimulationHeader';
 import SimulationForm from '../components/SimulationForm';
 import VehicleCard from '../components/VehicleCard';SimulatedValues
 import SimulatedValues from '../components/SimulatedValues';
 import Footer from '../components/Footer';
-import { useForm } from 'react-hook-form';
-
 
 export default function Home() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
 
-  const onSubmit = data => console.log(data);
   return (
     <main
       className={`flex min-h-screen flex-col `}
     >
         <Header />
         <SimulationHeader />
-        <SimulationForm onSubmit={handleSubmit} register={register} />
+        <SimulationForm />
         
       <div className="lg:flex  max-sm:space-y-6 lg:space-x-6 mb-24 px-6">
         <VehicleCard />
